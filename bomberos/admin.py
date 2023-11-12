a8bacfc073f294986b1937b90c11aa762fe22acf
krivanok1981 <146397476+krivanok1981@users.noreply.github.com>
2023-11-11 22:20:48 -0300

    admin.py
    se han importado los modelos de registro en la base de datos sql

--- bomberos/admin.py
+++ bomberos/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Articulos
+from .models import BodegaCentral
+from .models import Distribucion
+from .models import InvVeh
+from .models import Personal
+from .models import TipoUsuario
+from .models import Vehiculo
+from .models import BomberosArticulos
 
 admin.site.register(Articulos)
+admin.site.register(BodegaCentral)
+admin.site.register(BomberosArticulos)
+admin.site.register(Distribucion)
+admin.site.register(InvVeh)
+admin.site.register(Personal)
+admin.site.register(TipoUsuario)
+admin.site.register(Vehiculo)
